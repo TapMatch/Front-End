997f79bb97816cf9242dcfb69117c297c3be7671
Vladimir Radovanovich <vlad.radovanovich@gmail.com>
2020-11-24 21:27:53 +0100

    forgot this

---
+++ ts/UIConfig/fonts.ts
@@ -0,0 +1,10 @@
+export const fonts = {
+  bold: 'D-DIN-Bold',
+  italic: 'D-DIN-Italic',
+  regular: 'D-DIN',
+  cBold: 'D-DINCondensed-Bold',
+  cRegular: 'D-DINCondensed',
+  eBold: 'D-DINExp-Bold',
+  eItalic: 'D-DINExp-Italic',
+  eRegular: 'D-DINExp',
+};
--- ts/app/LoggedOut/screens/LoggedOutPlaceholderScreen.tsx
+++ ts/app/LoggedOut/screens/LoggedOutPlaceholderScreen.tsx
@@ -4,14 +4,14 @@ import SvgTest from 'ts/app/common/components/SvgTest';
 import TapMatchLogoRed from 'assets/svg/TapMatchLogo-red.svg';
 
 import {TapMatchContext} from 'ts/app/contexts/TapMatchContext';
-
+import {fonts as _f} from 'ts/UIConfig/fonts';
 interface LoggedOutPlaceholderScreenProps {}
 
 const LoggedOutPlaceholderScreen = (props: LoggedOutPlaceholderScreenProps) => {
   const {LoggedIn} = useContext(TapMatchContext);
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'blue'}}>
-      <Text>LoggedOutPlaceholderScreen</Text>
+      <Text style={_s.txt}>LoggedOutPlaceholderScreen</Text>
       <Button title={'Log In'} onPress={() => LoggedIn[1](true)} />
       <SvgTest />
       <TapMatchLogoRed width={220} height={220} />
@@ -21,6 +21,7 @@ const LoggedOutPlaceholderScreen = (props: LoggedOutPlaceholderScreenProps) => {
 
 export default LoggedOutPlaceholderScreen;
 
-const styles = StyleSheet.create({
+const _s = StyleSheet.create({
   container: {flex: 1, backgroundColor: 'blue', height: 500},
+  txt: {fontFamily: _f.italic, fontSize: 40},
 });
