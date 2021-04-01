07094d03e64c632f19db654a56fa78bbd41f23c7
Vladimir <vlad.radovanovich@gmail.com>
2021-04-01 21:17:14 +0200

    fix for branch share link

--- ts/app/common/services/shareContent.ts
+++ ts/app/common/services/shareContent.ts
@@ -1,65 +1,69 @@
-import {Platform, Share} from "react-native";
-import callAlert from "ts/utils/callAlert";
+import {Platform, Share} from 'react-native';
+import callAlert from 'ts/utils/callAlert';
 import branch from 'react-native-branch';
-import {constants} from "ts/constants/constants";
+import {constants} from 'ts/constants/constants';
 
 const shareContent = async (data: any, userProfile: any) => {
-    try {
-        let branchUniversalObject = await branch.createBranchUniversalObject(`event-${data.name}-${data.id}`, {
-            locallyIndex: true,
-            title: 'TapMatch',
-            contentDescription: 'TapMatch',
-            contentMetadata: {
-                customMetadata: {
-                    tapmatch_community_id: `${data.community_id}`,
-                    tapmatch_event_data: JSON.stringify(data),
-                }
-            }
-        });
-        let linkProperties = {
-            feature: 'share',
-            channel: `TapMatch ${Platform.OS} app`
-        };
-        let controlParams = {
-            $url_redirect_mode: 2,
-            $deeplink_path: 'Home',
-            $desktop_url: constants.feedbackURL
-        };
-        let {url} = await branchUniversalObject.generateShortUrl(linkProperties, controlParams);
+  try {
+    let branchUniversalObject = await branch.createBranchUniversalObject(
+      `event-${data.name}-${data.id}`,
+      {
+        locallyIndex: true,
+        title: 'TapMatch',
+        contentDescription: 'TapMatch',
+        contentMetadata: {
+          customMetadata: {
+            tapmatch_community_id: `${data.community_id}`,
+            tapmatch_event_data: JSON.stringify(data),
+          },
+        },
+      },
+    );
+    let linkProperties = {
+      feature: 'share',
+      channel: `TapMatch ${Platform.OS} app`,
+    };
+    let controlParams = {
+      $url_redirect_mode: 2,
+      $deeplink_path: 'Home',
+      $desktop_url: constants.websiteURL,
+    };
+    let {url} = await branchUniversalObject.generateShortUrl(
+      linkProperties,
+      controlParams,
+    );
 
-        const community = userProfile[0].communities[0].find((el: any) => el.id === data.community_id);
+    const community = userProfile[0].communities[0].find(
+      (el: any) => el.id === data.community_id,
+    );
 
-        if (community) {
-            const {
-                access: communityAccessCode,
-                name: communityName
-            } = community;
-            const result = await Share.share({
-                message: generateMsg(url, communityAccessCode, communityName),
-            });
-            if (result.action === Share.sharedAction) {
-                if (result.activityType) {
-                    console.log(result, 'result-on');
-                } else {
-                    console.log(result, 'result-off');
-                }
-            } else if (result.action === Share.dismissedAction) {
-                console.log('dismissedAction');
-            }
+    if (community) {
+      const {access: communityAccessCode, name: communityName} = community;
+      const result = await Share.share({
+        message: generateMsg(url, communityAccessCode, communityName),
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log(result, 'result-on');
+        } else {
+          console.log(result, 'result-off');
         }
-    } catch (error) {
-        console.log(error, '::: shareContent');
-        callAlert(undefined, error.message);
+      } else if (result.action === Share.dismissedAction) {
+        console.log('dismissedAction');
+      }
     }
+  } catch (error) {
+    console.log(error, '::: shareContent');
+    callAlert(undefined, error.message);
+  }
 };
 
-
 function generateMsg(url: string, code: string, name: string) {
-    if (code) {
-        return `${name}\nJoin our event!\nThe access code to enter the community is ${code} \n ${url}`;
-    } else {
-        return `${name}\nJoin our event!\nIt's an open invitation\n ${url}`;
-    }
+  if (code) {
+    return `${name}\nJoin our event!\nThe access code to enter the community is ${code} \n ${url}`;
+  } else {
+    return `${name}\nJoin our event!\nIt's an open invitation\n ${url}`;
+  }
 }
 
 export default shareContent;
--- ts/constants/constants.ts
+++ ts/constants/constants.ts
@@ -5,6 +5,7 @@ export const constants = {
   privacyPolicy_EN: 'https://www.tapmatchapp.com/general-2',
   requestCommunityURL: 'https://www.tapmatchapp.com/community',
   feedbackURL: 'https://www.tapmatchapp.com/support',
+  websiteURL: 'https://www.tapmatchapp.com',
 };
 
 export const tapMatchServerUrl = DEV_MODE
